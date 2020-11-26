d543aecae2454f402018b5ad184d79d27d6ed112
luidimso <luidi_mso@outlook.com>
2020-11-26 10:22:22 -0300

    Starting layout

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,40 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Task List"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        labelText: "New task",
+                        labelStyle: TextStyle(
+                            color: Colors.blueAccent
+                        )
+                    ),
+                  )
+                ),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  child: Text("Add"),
+                  textColor: Colors.white,
+                  onPressed: () {}
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   Future<File> _getFile() async {
